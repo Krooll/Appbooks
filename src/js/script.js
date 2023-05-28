fcea69ce5d1e2042b793cae1dc530fa4b22f0be8
Pawel Krol <paullkroll@icloud.com>
2023-05-28 18:56:45 +0200

    FilterBooks functions done, add determineRatingBgc function and modify html book-rating-fill - add style

--- src/js/script.js
+++ src/js/script.js
@@ -32,6 +32,7 @@
       thisBook.getElements();
       thisBook.render();
       thisBook.initActions();
+      thisBook.filterBooks();
     }
 
     getElements(){
@@ -46,6 +47,10 @@
       const thisBook = this;
 
       for(let bookId of dataSource.books){
+        const ratingBgc = thisBook.determineRatingBgc(bookId.rating);
+        const ratingWidth = 10 * bookId.rating;
+        bookId.ratingBgc = ratingBgc;
+        bookId.ratingWidth = ratingWidth;
         const generatedHTML = templates.bookLists(bookId);
         const generatedDOM = utils.createDOMFromHTML(generatedHTML); 
         thisBook.container.appendChild(generatedDOM);
@@ -91,6 +96,8 @@
             filters.push(filter.value);
           }
         }
+
+        thisBook.filterBooks();
       });
     }
 
@@ -98,13 +105,31 @@
       const thisBook = this; 
 
       for(const book of dataSource.books){
-        let shouldBeHidden = false; 
-        for(const filter of filters){
+        thisBook.shouldBeHidden = false; 
+        const cover = document.querySelector('.book__image[data-id="'+ book.id+'"]');
+        for(const filter of filters) {
           if(!book.details[filter]){
-            
+            thisBook.shouldBeHidden = true;
+            break;
           }
         }
+        if(thisBook.shouldBeHidden){                                                  
+          cover.classList.add('hidden');                                 
+        } else {
+          cover.classList.remove('hidden');                                   
+        }
       }
+    }
+
+    determineRatingBgc(rating){
+      const thisBook = this; 
+      thisBook.rating = '';
+      if(rating < 6) thisBook.rating = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%);';
+      if(rating > 6 && rating <= 8) thisBook.rating = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%);';
+      if(rating > 8 && rating <=9) thisBook.rating = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%);';
+      if(rating > 9) thisBook.rating = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%);';
+
+      return thisBook.rating;
     }
   }
 
